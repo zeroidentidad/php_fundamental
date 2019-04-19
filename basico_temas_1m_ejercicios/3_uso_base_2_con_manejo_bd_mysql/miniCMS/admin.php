bf7a5c9ec88d869de12731927254d0e3c65a35bb
Jesus Ferrer <jafs-2050@hotmail.com>
2019-04-19 00:37:52 -0500

    avance agregar ejercicios 1m basicos 3

---
+++ basico_temas_1m_ejercicios/3_uso_base_2_con_manejo_bd_mysql/miniCMS/admin.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Modulo Admin</title>
+	<link rel="stylesheet" type="text/css" href="./css/estilos.css">
+</head>
+<body>
+	<div id="cabecera">
+		<h1>Cursos</h1>
+	</div>
+	<div id="contenido">
+		<table border="1" id="estructura">
+			<tr>
+				<td id="menu">&nbsp;</td>
+				<td id="pagina">
+					<h3>Administración</h3>
+					<ul>
+						<li><a href="contenido.php">Administrar contenidos</a></li>
+						<li><a href="nuevo_usuario.php">Crear usuarios</a></li>
+						<li><a href="salir.php">Salir</a></li>
+					</ul>
+				</td>
+			</tr>
+		</table>
+	</div>
+	<div id="pie">
+		<p>Derechos o Izquierdos</p>
+	</div>
+</body>
+</html>
---
+++ basico_temas_1m_ejercicios/3_uso_base_2_con_manejo_bd_mysql/miniCMS/bd/ejercicio1ms3.sql
@@ -0,0 +1,110 @@
+-- Servidor: localhost:3306
+-- Tiempo de generación: 18-04-2019 a las 22:35:09
+-- Versión del servidor: 5.7.23
+-- Versión de PHP: 7.0.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `ejercicio1ms3`
+--
+CREATE DATABASE IF NOT EXISTS `ejercicio1ms3` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci;
+USE `ejercicio1ms3`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `capitulos`
+--
+
+CREATE TABLE `capitulos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `posicion` int(3) NOT NULL,
+  `visibilidad` tinyint(1) NOT NULL,
+  `contenido` text COLLATE utf8_spanish_ci NOT NULL,
+  `curso_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cursos`
+--
+
+CREATE TABLE `cursos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `posicion` int(3) NOT NULL,
+  `visibilidad` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `usuario` int(30) NOT NULL,
+  `contrasena` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `capitulos`
+--
+ALTER TABLE `capitulos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `cursos`
+--
+ALTER TABLE `cursos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `capitulos`
+--
+ALTER TABLE `capitulos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cursos`
+--
+ALTER TABLE `cursos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
