28ef29409ab34cf087911450c2a12da745c47267
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-08-15 16:57:11 -0500

    webcom
    boton invitacion acceso total al server

--- getin.mywebcommunity.org/web/web/chat.php
+++ getin.mywebcommunity.org/web/web/chat.php
@@ -2,12 +2,19 @@
 require_once './header.php';
 ?>
 <div class="full-height">
-    <p class="p-center f20"><b>Chat server para ser ecosistema y punto de encuentro entre:</b></p>
-    <p class="p-center">Usuarios, Programadores Web, ‌‌Diseñadores, ‌‌Editores, Animadores, Marketers, Testers, DBAs, Formadores, Artistas, Tecnicos, Full Stacks, ‌‌Programadores Mobile, ‌‌Programadores Desktop, Programadores Games, Webmasters, Emprendedores, Empleadores, ETC.</p>
-    <p class="p-center f16"><b><u>Miembros activos:</u></b></p>
-    <iframe src="https://discordapp.com/widget?id=531918618670268420&theme=dark" class="full-height" width="100%" allowtransparency="true" frameborder="0">
-    </iframe>
-</div>    
+	<p class="p-center f20"><b>Chat server para ser ecosistema y punto de encuentro entre:</b></p>
+	<p class="p-center">Usuarios, Programadores Web, ‌‌Diseñadores, ‌‌Editores, Animadores, Marketers, Testers, DBAs, Formadores, Artistas, Tecnicos, Full Stacks, ‌‌Programadores Mobile, ‌‌Programadores Desktop, Programadores Games, Webmasters, Emprendedores, Empleadores, ETC.</p>
+	<p class="p-center f16"><b><u>Miembros activos:</u></b></p>
+	<iframe src="https://discordapp.com/widget?id=531918618670268420&theme=dark" class="full-height" width="100%" allowtransparency="true" frameborder="0">
+	</iframe>
+</div>
+<div class="row text-center">
+	<div class="col col-sm-12 col-md-12 col-lg-12">
+		<button class="btn btn-primary" type="button" onclick="javascript:location.href='https://discord.gg/nrrYxtp';">
+		<b>Invitación acceso total</b>
+		</button>
+	</div>		
+</div>
 <?php
 require_once './footer.php';
 ?>
