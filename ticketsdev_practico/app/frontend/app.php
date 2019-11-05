004b84c16ad1d2ae9ba74be5899feae68217ec84
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-11-04 20:28:37 -0600

    tickets app
    funciones obtener horarios y existencia registros

--- ticketsdev_practico/app/frontend/app.php
+++ ticketsdev_practico/app/frontend/app.php
@@ -16,6 +16,43 @@ function obtener_cupo($id_actividad){
     return $result;
 }
 
-echo '<pre>';
+function obtener_horarios($disciplina){
+    $sql = "SELECT * FROM actividades WHERE disciplina=? ORDER BY id_actividad";
+
+    $data = array($disciplina);
+
+    $result = db_query($sql,$data,true);
+
+    if (count($result)===0) {
+        echo 'No hay horarios '.$disciplina;
+    } else {
+        echo '<pre>';
+        var_dump($result);
+        echo '</pre>';
+    }
+
+    return $result;    
+}
+
+function existe_registro($email){
+    $sql = "SELECT p.email, p.nombre, p.apellido, a.bloque, a.disciplina, a.horario, r.fecha
+            FROM registro AS r INNER JOIN actividades AS a ON a.id_actividad = r.id_actividad
+            INNER JOIN participantes AS p ON p.email = r.email
+            WHERE r.email = ?";
+
+    $data = array($email);
+
+    $result = db_query($sql, $data, true, true);
+
+    return $result;
+}
+
+/*echo '<pre>';
 var_dump(obtener_cupo('1B'));
+echo '</pre>';*/
+
+//obtener_horarios('BACKEND');
+
+echo '<pre>';
+var_dump(existe_registro('test@mail.com'));
 echo '</pre>';
