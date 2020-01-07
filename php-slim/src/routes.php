c836b63289f438f10516efbe1cd80db6201774d0
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-01-06 22:15:47 -0600

    php slim 3 rutas
    ej rutas y agrupado

--- php-slim/src/routes.php
+++ php-slim/src/routes.php
@@ -11,3 +11,55 @@ $app->get('/[{name}]', function ($request, $response, $args) {
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+// rutas agrupadas
+$app->group('/users', function () {
+    $this->get('/getAll', function ($request, $response, $args) {
+        return 'Todos los users';
+    });
+
+    $this->get('/get/{id:[0-9]+}', function ($request, $response, $args) {
+        return 'Get usuario ' . $args['id'];
+    });
+
+    $this->post('/insert', function ($request, $response, $args) {
+        return 'Nuevo usuario creado';
+    });
+
+    $this->put('/update', function ($request, $response, $args) {
+        return 'Usuario actualizado';
+    });
+
+    $this->delete('/delete/{id:[0-9]+}', function ($request, $response, $args) {
+        return 'Delete usuario ' . $args['id'];
+    });
+
+    $this->any('/index', function ($request, $response, $args) {
+        return 'Mediante cualquier metodo';
+    });
+});
+
+// rutas aisladas
+$app->get('/dogs/getAll', function ($request, $response, $args) {
+    return 'Todos los dogs';
+});
+
+$app->get('/dogs/get/{id:[0-9]+}', function ($request, $response, $args) {
+    return 'Get perro '.$args['id'];
+});
+
+$app->post('/dogs/insert', function ($request, $response, $args) {
+    return 'Nuevo perro creado';
+});
+
+$app->put('/dogs/update', function ($request, $response, $args) {
+    return 'Perro actualizado';
+});
+
+$app->delete('/dogs/delete/{id:[0-9]+}', function ($request, $response, $args) {
+    return 'Delete perro '.$args['id'];
+});
+
+$app->any('/dogs/index', function ($request, $response, $args) {
+    return 'Mediante cualquier metodo';
+});
