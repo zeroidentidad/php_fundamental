d2c2720cccec3212c661cbcae33ce1ea49ab9d2b
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-02-03 20:31:10 -0600

    api pedidos
    init assets, inncesario last

--- xcodeigniter-backoffice/application/views/footer.php
+++ xcodeigniter-backoffice/application/views/footer.php
@@ -1,4 +1,12 @@
+            <div class="well well-sm text-center col-xs-12">
+                By <a href="https://zeroidentidad.github.io" target="_blank">Zero</a>
+            </div>    
+        </div>
+    </div>
+</div>
 
+    <script src="<?php echo base_url('assets/js/jquery-2.2.1.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
 </body>
 
 </html>
--- xcodeigniter-backoffice/application/views/home/otro.php
+++
@@ -1,7 +0,0 @@
-<div class="container">
-    <div class="row">
-        <div class="col-xs-12">
-            <h1 class="page-header">En otro</h1>
-        </div>
-    </div>
-</div>
