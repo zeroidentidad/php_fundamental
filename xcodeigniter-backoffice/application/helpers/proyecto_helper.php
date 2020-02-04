6c282fb61a266de98634d295119642b24c18f930
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-02-03 19:46:32 -0600

    api pedidos
    modificaciones

--- xcodeigniter-backoffice/application/helpers/proyecto_helper.php
+++
@@ -1,5 +0,0 @@
-<?php
-function HolaMundo(){
-    return 'Hola Mundo';
-}
-?>
--- xcodeigniter-backoffice/application/libraries/Proyecto.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Proyecto {
-    public function realizaAlgo()
-    {
-        return 'Haciendo algo';
-    }
-}
-?>
